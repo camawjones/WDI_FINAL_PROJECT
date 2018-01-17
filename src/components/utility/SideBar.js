f1be64ce1cc8852e052c60a4593e602e0c8cf481
camawjones <camawjones@gmail.com>
2018-01-17 18:19:25 +0000

    pushing to heroku again

--- src/components/utility/SideBar.js
+++ src/components/utility/SideBar.js
@@ -1,4 +1,4 @@
-import React                from 'react';
+import React  from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../lib/Auth';
 import { withRouter } from 'react-router-dom';
